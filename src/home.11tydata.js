afe8dafc5b9ec16f4112a5654fd80a666d63da6a
Trevor Morris <code@trovster.com>
2026-05-14 16:36:11 +0100

    fix: add pagination to the photo collection

---
+++ src/home.11tydata.js
@@ -0,0 +1,15 @@
+import photos from "./data/photos.js"
+
+export default {
+    pagination: {
+        data: "collections.photos",
+        size: photos.pagination,
+        alias: "paginatedPhotos",
+    },
+    permalink: ({ pagination }) => pagination.pageNumber > 0 ? `/page/${pagination.pageNumber + 1}/` : "/",
+    eleventyComputed: {
+        title: ({ pagination, site }) => pagination.pageNumber > 0 ? `Page ${pagination.pageNumber + 1 }` : site.title,
+        description: () => "Favourite photos by Trevor Morris.",
+        bodyClass: () => "page page-home",
+    },
+}
