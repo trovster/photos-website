c93e8636197e823b1bb5ebe6370f191d5d87f402
Trevor Morris <code@trovster.com>
2026-05-18 11:31:22 +0100

    fix: tweaks to about content

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,7 +1,7 @@
 export default {
     name: "Joe Bloggs",
     location: "Placename, Country",
-    bio: "This is my personal photography website archive and it showcases my favourite photos. Built with 11ty.",
+    bio: "This is an example personal photography website. It is built with the static-site generator, 11ty, and includes widgets such as colour palette and EXIF.",
     website: "",
     avatar: {
         src: "",
